812bc6773bee1e75120d2dece164fa3184b07578
Aditya Saxena <60032144+asaxena012@users.noreply.github.com>
2021-03-07 22:34:58 +0530

    animations: added aos, typewriting effect on landing component

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Landing.css";
 import heroImage from "./../../assets/hero.svg";
+import Typewriter from "typewriter-effect";
 
 const Landing = () => {
   return (
     <div className="landing-container">
-      <div className="landing-left">
+      <div data-aos="fade-right" className="landing-left">
         <h1 className="hero-text">Can you type...</h1>
-        <p className="typewriter-text">quick?</p>
+        <div className="typewriter-container">
+          <Typewriter
+            options={{
+              strings: ["quick?", "correct?", "in speed?"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        {/* <p className="typewriter-text">quick?</p> */}
         {/* <p className="typewriter-text">fast?</p>
         <p className="typewriter-text">in speed?</p> */}
       </div>
@@ -16,6 +26,7 @@ const Landing = () => {
           className="hero-image"
           src={heroImage}
           alt="Image of a man running"
+          data-aos="fade-left"
         />
       </div>
     </div>
