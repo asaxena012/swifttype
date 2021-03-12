98e8535609dfd020c49367bc09c8c78f4aa67766
Aditya Saxena <60032144+asaxena012@users.noreply.github.com>
2021-03-12 20:11:02 +0530

    Revert "component: ChallengeSection Component initiated"
    This reverts commit eaa1193fbefdae58b9dab79b0a016c91958f90b0.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Nav from "./../Nav/Nav";
 import Landing from "./../Landing/Landing";
 import Footer from "./../Footer/Footer";
-import ChallengeSection from "./../ChallengeSection/ChallengeSection";
 
 class App extends React.Component {
   render() {
@@ -11,7 +10,6 @@ class App extends React.Component {
       <div className="app">
         <Nav />
         <Landing />
-        <ChallengeSection />
         <Footer />
       </div>
     );
